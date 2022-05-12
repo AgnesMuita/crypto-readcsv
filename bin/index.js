d343c166d790431235fefd448c0964eebd35d5be
Agnes Muita <agnes.muita@alternateltd.com>
2022-05-12 09:24:38 +0300

    calculate token values using exchange values from cryptocompare api

--- bin/index.js
+++ bin/index.js
@@ -2,7 +2,12 @@ const csv = require('csv-parser')
 const fs = require('fs')
 const start = Date.now()
 const items = []
-let portfolioAmount = 0
+let BTCportfolioAmount = 0;
+let ETHportfolioAmount = 0;
+let XRPportfolioAmount = 0;
+let BTCvalue;
+let ETHvalue;
+let XRPvalue;
 
 
 //obtain exchange rates 
@@ -12,12 +17,24 @@ const { notStrictEqual } = require("assert");
 const { showCompletionScript, array } = require("yargs");
 const { time } = require("console");
 cc.setApiKey('10c5bbea1ecc63121cf5c8805e48708350cacd20857351d34892f57747dafa7f')
- 
 cc.priceMulti(['BTC', 'ETH','XRP'], ['USD'])
-.then(prices => {
-  // console.log(prices)
-})
-.catch(console.error)
+        .then(prices => {
+        //BTC price
+        for (var property in prices.BTC){
+          BTCvalue = prices.BTC[property]
+        }
+        //ETH price
+        for (var property in prices.ETH){
+          ETHvalue = prices.ETH[property]
+        }
+        //XRP price
+        for (var property in prices.XRP){
+          XRPvalue = prices.XRP[property]
+        }
+        })
+        .catch(console.error);
+
+
 
 //read csv file data
 fs.createReadStream('transactions.csv', {})
@@ -25,29 +42,31 @@ fs.createReadStream('transactions.csv', {})
     .on('data', chunk => {
         //get ethereum
         if(chunk.token === "ETH" && chunk.transaction_type === "DEPOSIT"){
-            portfolioAmount += parseFloat(chunk.amount)
+            BTCportfolioAmount += parseFloat(chunk.amount)
         }else if(chunk.token === "ETH" && chunk.transaction_type === "WITHDRAWAL"){
-            portfolioAmount -= parseFloat(chunk.amount)
+            BTCportfolioAmount -= parseFloat(chunk.amount)
         }
         //get BTC
         if(chunk.token === "BTC" && chunk.transaction_type === "DEPOSIT"){
-            portfolioAmount += parseFloat(chunk.amount)
+            ETHportfolioAmount += parseFloat(chunk.amount)
         }else if(chunk.token === "BTC" && chunk.transaction_type === "WITHDRAWAL"){
-            portfolioAmount -= parseFloat(chunk.amount)
+            ETHportfolioAmount -= parseFloat(chunk.amount)
         }
         //get XRP
         if(chunk.token === "XRP" && chunk.transaction_type === "DEPOSIT"){
-            portfolioAmount += parseFloat(chunk.amount)
+            XRPportfolioAmount += parseFloat(chunk.amount)
         }else if(chunk.token === "XRP" && chunk.transaction_type === "WITHDRAWAL"){
-            portfolioAmount -= parseFloat(chunk.amount)
+            XRPportfolioAmount -= parseFloat(chunk.amount)
         }
-
     })
     .on('error', err => {
         console.log(err)
     })
     .on('end', () => {
-        console.log(`ETH portfolio value is ${portfolioAmount}`)
+       
+        console.log(`ETH portfolio value is ${BTCportfolioAmount * BTCvalue}`)
+        console.log(`BTC portfolio value is ${ETHportfolioAmount * ETHvalue}`)
+        console.log(`XRP portfolio value is ${XRPportfolioAmount * XRPvalue}`)
         const now = Date.now()
         const tat = now - start
         console.log(`${tat/1000} seconds`)

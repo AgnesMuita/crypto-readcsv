5ee8a7d1d92df884fe834337d589b1d3469a1fc3
Agnes Muita <agnes.muita@alternateltd.com>
2022-05-12 14:04:41 +0300

    add commandline arguments

--- bin/index.js
+++ bin/index.js
@@ -1,6 +1,7 @@
 const csv = require('csv-parser')
 const fs = require('fs')
 const start = Date.now()
+const yargs = require("yargs")
 const items = []
 let BTCportfolioAmount = 0;
 let ETHportfolioAmount = 0;
@@ -8,6 +9,10 @@ let XRPportfolioAmount = 0;
 let BTCvalue;
 let ETHvalue;
 let XRPvalue;
+let inputDate = "24 May 2022";
+let timestampDate = Math.floor(new Date(inputDate).getTime()) / 1000;
+let timeArray = [];
+
 
 
 //obtain exchange rates 
@@ -34,13 +39,11 @@ cc.priceMulti(['BTC', 'ETH','XRP'], ['USD'])
         })
         .catch(console.error);
 
-
-
 //read csv file data
-fs.createReadStream('transactions.csv', {})
+    fs.createReadStream('transactions.csv', {})
     .pipe(csv())
     .on('data', chunk => {
-        //get ethereum
+        //get ETH
         if(chunk.token === "ETH" && chunk.transaction_type === "DEPOSIT"){
             BTCportfolioAmount += parseFloat(chunk.amount)
         }else if(chunk.token === "ETH" && chunk.transaction_type === "WITHDRAWAL"){
@@ -58,17 +61,39 @@ fs.createReadStream('transactions.csv', {})
         }else if(chunk.token === "XRP" && chunk.transaction_type === "WITHDRAWAL"){
             XRPportfolioAmount -= parseFloat(chunk.amount)
         }
+
     })
     .on('error', err => {
         console.log(err)
     })
     .on('end', () => {
-       
-        console.log(`ETH portfolio value is ${BTCportfolioAmount * BTCvalue}`)
-        console.log(`BTC portfolio value is ${ETHportfolioAmount * ETHvalue}`)
-        console.log(`XRP portfolio value is ${XRPportfolioAmount * XRPvalue}`)
+      //command line arguments
+      if(process.argv >= 2 ){
+           process.argv.slice(2).forEach(function (token,amount, array) {
+            if(token === "token=ETH"){
+              amount= BTCportfolioAmount * BTCvalue
+              console.log(`BTC portfolio value: ${amount}`);  
+            }else if(token === "token=BTC"){
+              amount= ETHportfolioAmount * ETHvalue
+              console.log(`ETH portfolio value: ${amount}`);
+            }else if(token === "token=XRP"){
+              amount= XRPportfolioAmount * XRPvalue
+              console.log(`XRP portfolio value: ${amount}`);
+            }
+          });
+      }else{     
+          console.log(`ETH portfolio value is ${BTCportfolioAmount * BTCvalue} USD`)
+          console.log(`BTC portfolio value is ${ETHportfolioAmount * ETHvalue} USD`)
+          console.log(`XRP portfolio value is ${XRPportfolioAmount * XRPvalue} USD`)
+        }
+    
+
         const now = Date.now()
         const tat = now - start
         console.log(`${tat/1000} seconds`)
         console.log("done")
     })
+
+
+
+
